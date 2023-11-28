500872114ef6cef35c04c07fbf4d90c716598d56
Chandrababugowda <chandrababugowda3042003@gmail.com>
2023-11-28 08:35:48 +0530

    Change the hero colors

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('show--animation');
-        }else{
-            entry.target.classList.remove('show--animation');
-        }
-    })
-})
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show--animation");
+    } else {
+      entry.target.classList.remove("show--animation");
+    }
+  });
+});
 
-const hiddenElements = document.querySelectorAll('.hidden--animation');
-hiddenElements.forEach((el)=>{
-    observer.observe(el);
-})
+const hiddenElements = document.querySelectorAll(".hidden--animation");
+hiddenElements.forEach((el) => {
+  observer.observe(el);
+});
+
+const blurDivs = document.querySelectorAll(".blur-load");
+blurDivs.forEach((div) => {
+  const img = document.querySelector("img");
+
+  function loaded() {
+    div.classList.add("loaded");
+  }
+
+  if (img.complete) {
+    loaded();
+  } else {
+    img.addEventListener("load", loaded);
+  }
+});
